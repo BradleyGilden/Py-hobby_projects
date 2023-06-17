77b321653f4b21612180a50c3ef81087368c3646
BradleyGilden <bradleygilden@gmail.com>
2023-06-17 18:30:51 +0200

    updated keys

--- pygame/keyhandle.py
+++ pygame/keyhandle.py
@@ -1,13 +1,13 @@
 from globals import VEL, WIDTH, HEIGHT
-import pygame
+from pygame import K_LSHIFT, K_w, K_a, K_d, K_s, K_INSERT
+from pygame import K_LEFT, K_RIGHT, K_DOWN, K_UP
 
 
-def handle_keys_yellow(yellow):
-    key_pressed = pygame.key.get_pressed()  # get pressed keys from user
+def handle_keys_yellow(yellow, key_pressed):
     boost = 1
-    if key_pressed[pygame.K_LSHIFT]:  # check for boost key
+    if key_pressed[K_LSHIFT]:  # check for boost key
         boost = 3
-    if key_pressed[pygame.K_w] and key_pressed[pygame.K_a]:  # UP/LEFT
+    if key_pressed[K_w] and key_pressed[K_a]:  # UP/LEFT
         yellow.y -= VEL * boost
         if yellow.y < 0:
             yellow.y = 0
@@ -15,7 +15,7 @@ def handle_keys_yellow(yellow):
         if yellow.x < 0:
             yellow.x = 0
         return
-    elif key_pressed[pygame.K_w] and key_pressed[pygame.K_d]:
+    elif key_pressed[K_w] and key_pressed[K_d]:  # UP/ RIGHT
         yellow.y -= VEL * boost
         if yellow.y < 0:
             yellow.y = 0
@@ -23,7 +23,7 @@ def handle_keys_yellow(yellow):
         if yellow.x > WIDTH/2 - 75:
             yellow.x = WIDTH/2 - 75
         return
-    elif key_pressed[pygame.K_s] and key_pressed[pygame.K_a]:
+    elif key_pressed[K_s] and key_pressed[K_a]:  # DOWN/LEFT
         yellow.y += VEL * boost
         if yellow.y > HEIGHT - 82:
             yellow.y = HEIGHT - 82
@@ -31,7 +31,7 @@ def handle_keys_yellow(yellow):
         if yellow.x < 0:
             yellow.x = 0
         return
-    elif key_pressed[pygame.K_s] and key_pressed[pygame.K_d]:
+    elif key_pressed[K_s] and key_pressed[K_d]:  # DOWN/RIGHT
         yellow.y += VEL * boost
         if yellow.y > HEIGHT - 82:
             yellow.y = HEIGHT - 82
@@ -39,34 +39,33 @@ def handle_keys_yellow(yellow):
         if yellow.x > WIDTH/2 - 75:
             yellow.x = WIDTH/2 - 75
         return
-    elif key_pressed[pygame.K_a]:  # LEFT
+    elif key_pressed[K_a]:  # LEFT
         yellow.x -= VEL * boost
         if yellow.x < 0:
             yellow.x = 0
         return
-    elif key_pressed[pygame.K_w]:  # UP
+    elif key_pressed[K_w]:  # UP
         yellow.y -= VEL * boost
         if yellow.y < 0:
             yellow.y = 0
         return
-    elif key_pressed[pygame.K_d]:  # RIGHT
+    elif key_pressed[K_d]:  # RIGHT
         yellow.x += VEL * boost
         if yellow.x > WIDTH/2 - 75:
             yellow.x = WIDTH/2 - 75
         return
-    elif key_pressed[pygame.K_s]:  # Down
+    elif key_pressed[K_s]:  # Down
         yellow.y += VEL * boost
         if yellow.y > HEIGHT - 82:
             yellow.y = HEIGHT - 82
         return
 
 
-def handle_keys_red(red):
-    key_pressed = pygame.key.get_pressed()  # get pressed keys from user
+def handle_keys_red(red, key_pressed):
     boost = 1
-    if key_pressed[pygame.K_RCTRL]:  # check for boost key
+    if key_pressed[K_INSERT]:  # check for boost key
         boost = 3
-    if key_pressed[pygame.K_UP] and key_pressed[pygame.K_LEFT]:  # UP/LEFT
+    if key_pressed[K_UP] and key_pressed[K_LEFT]:  # UP/LEFT
         red.x -= VEL * boost
         red.y -= VEL * boost
         if red.y < 0:
@@ -74,7 +73,7 @@ def handle_keys_red(red):
         if red.x < WIDTH/2 + 5:
             red.x = WIDTH/2 + 5
         return
-    elif key_pressed[pygame.K_UP] and key_pressed[pygame.K_RIGHT]:
+    elif key_pressed[K_UP] and key_pressed[K_RIGHT]:  # UP/RIGHT
         red.y -= VEL * boost
         red.x += VEL * boost
         if red.x > WIDTH-70:
@@ -82,7 +81,7 @@ def handle_keys_red(red):
         if red.y < 0:
             red.y = 0
         return
-    elif key_pressed[pygame.K_DOWN] and key_pressed[pygame.K_LEFT]:
+    elif key_pressed[K_DOWN] and key_pressed[K_LEFT]:  # DOWN/LEFT
         red.y += VEL * boost
         red.x -= VEL * boost
         if red.x < WIDTH/2 + 5:
@@ -90,7 +89,7 @@ def handle_keys_red(red):
         if red.y > HEIGHT - 82:
             red.y = HEIGHT - 82
         return
-    elif key_pressed[pygame.K_DOWN] and key_pressed[pygame.K_RIGHT]:
+    elif key_pressed[K_DOWN] and key_pressed[K_RIGHT]:  # DOWN/RIGHT
         red.y += VEL * boost
         red.x += VEL * boost
         if red.x > WIDTH-70:
@@ -98,21 +97,21 @@ def handle_keys_red(red):
         if red.y > HEIGHT - 82:
             red.y = HEIGHT - 82
         return
-    elif key_pressed[pygame.K_LEFT]:  # LEFT
+    elif key_pressed[K_LEFT]:  # LEFT
         red.x -= VEL * boost
         if red.x < WIDTH/2 + 5:
             red.x = WIDTH/2 + 5
         return
-    elif key_pressed[pygame.K_UP]:  # UP
+    elif key_pressed[K_UP]:  # UP
         red.y -= VEL * boost
         if red.y < 0:
             red.y = 0
-    elif key_pressed[pygame.K_RIGHT]:  # RIGHT
+    elif key_pressed[K_RIGHT]:  # RIGHT
         red.x += VEL * boost
         if red.x > WIDTH-70:
             red.x = WIDTH-70
         return
-    elif key_pressed[pygame.K_DOWN]:  # DOWN
+    elif key_pressed[K_DOWN]:  # DOWN
         red.y += VEL * boost
         if red.y > HEIGHT - 82:
             red.y = HEIGHT - 82
--- pygame/main.py
+++ pygame/main.py
@@ -1,22 +1,32 @@
 import pygame
-from globals import WHITE, FPS, SS_H, SS_W
+from globals import WHITE, FPS, SS_H, SS_W, CENTER, RED, YELLOW
 from keyhandle import handle_keys_red, handle_keys_yellow
 from surfaces import M_WINDOW, Y_SPACESHIP_IMG, R_SPACESHIP_IMG
 from surfaces import BACKGROUND, BORDER
+from projectiles import bullets_setup, handle_bullets
 
 
 pygame.display.set_caption("Space Ship Shooter")
 
 
-def set_window(colour, yellow, red):
-    M_WINDOW.blit(BACKGROUND, (0, 0))
+def set_window(yellow, red, r_bullet, y_bullet):
+    M_WINDOW.blit(BACKGROUND, CENTER)
     pygame.draw.rect(M_WINDOW, WHITE, BORDER)
     M_WINDOW.blit(Y_SPACESHIP_IMG, (yellow.x, yellow.y))
     M_WINDOW.blit(R_SPACESHIP_IMG, (red.x, red.y))
+
+    for bullet in r_bullet:
+        pygame.draw.rect(M_WINDOW, RED, bullet)
+    for bullet in y_bullet:
+        pygame.draw.rect(M_WINDOW, YELLOW, bullet)
+
     pygame.display.update()
 
 
 def main():
+    bullet = []
+    y_bullet = []
+    r_bullet = []
     yellow = pygame.Rect(200, 200, SS_W, SS_H)
     red = pygame.Rect(700, 200, SS_W, SS_H)
     clock = pygame.time.Clock()
@@ -26,9 +36,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # checking events in real time
                 running = False
-        handle_keys_yellow(yellow)
-        handle_keys_red(red)
-        set_window(WHITE, yellow, red)
+        bullets_setup(event, yellow, red, y_bullet, r_bullet, bullet)
+        key_pressed = pygame.key.get_pressed()  # get pressed keys from user
+        handle_keys_yellow(yellow, key_pressed)
+        handle_keys_red(red, key_pressed)
+        handle_bullets(y_bullet, r_bullet, yellow, red)
+        set_window(yellow, red, y_bullet, r_bullet)
     pygame.quit()
 
 
