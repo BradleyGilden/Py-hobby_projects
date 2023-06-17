e519333c48c83d845483ab664f88bca724a0a140
BradleyGilden <bradleygilden@gmail.com>
2023-06-17 18:30:32 +0200

    more globals added

--- pygame/globals.py
+++ pygame/globals.py
@@ -1,9 +1,20 @@
+from pygame import USEREVENT
+
+
 WIDTH, HEIGHT = 1280, 720  # frame dimensions
+SS_H, SS_W = 85, 70  # space ship width and height
 VEL = 5  # velocity of ship
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+CENTER = (0, 0)
 FPS = 60
-SS_H, SS_W = 85, 70  # space ship width and height
-B_SPEED = 10
 
-bullets = []
+B_SPEED = 10
+MAX_BULLETS = 10000
+
+# COLLISION EVENTS
+Y_HIT = USEREVENT + 1
+R_HIT = USEREVENT + 2
